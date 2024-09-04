64a93cb8bf1e8ee89870a142479d6dc993b45b9b
SOHEL RANA <sohelrana104322@gmail.com>
2024-09-05 00:10:47 +0600

    update problem-solving(0) file

--- SQL Server/Problem Solving/problem-solving(0).sql
+++ SQL Server/Problem Solving/problem-solving(0).sql
@@ -1,5 +1,6 @@
 create database Employee;
 
+/*Create a table employee*/
 create table employee(
 worker_id int primary key,
 first_name varchar(20),
@@ -8,19 +9,23 @@ salary int,
 dept_name varchar(20),
 )
 
+/*Add a new column; JOINING_DATE to the existing relation.*/
 alter table employee add joining_date varchar(20);
 
+/*Change the datatype of SALARY.*/
 alter table employee alter column salary varchar(20)
 
+/*Modify the column width of the DEPARTMENT field of EMPLOYEE table*/
 alter table employee alter column dept_name varchar(30)
 
 
-
+/*Allow NULL for all columns except WORKER_ID.*/
 insert into employee (worker_id)
 values(222311057)
 
 select * from employee;
 
+/*Add constraints to check, while entering the SALARY value (i.e) SALARY > 100.*/
 alter table employee add check(salary > 100);
 
 insert into employee (salary)
@@ -29,7 +34,7 @@ values(50)
 insert into employee (salary)
 values(150)
 
-
+/*Define the field FIRST_NAME as UNIQUE. */
 alter table employee add unique(first_name);
 
 
@@ -42,11 +47,11 @@ truncate table employee;
 
 insert into employee(worker_id, first_name,last_name, salary, dept_name, joining_date)
 values
-(222311057, 'SOHEL', 'RANA', 20000, 'CSE', '20 SEPT 2024'),
-(222311053, 'FAHIM', 'RAHMAN', 20000, 'CSE', '20 SEPT 2024'),
-(222311076, 'SABIT', 'RAIHAN', 30000, 'CSE', '10 OCT 2023'),
-(2223110354, 'TAHSIN', 'SIDDIK', 424443, 'CSE', '2 OCT 2023'),
-(2231164, 'AKIB', 'AHMED', 32221, 'EEE', '3 JAN 1993'),
-(223123234, 'SOMRAT', 'BOY', 73554, 'BSC', '6 JUN 1971'),
-(222321, 'SHOWRAB', 'KHORMOKAR', 73554, 'DRUG D', '21 JAN 100'),
-(2221334, 'BIJOY' ,'MOHOITRO', 73534, 'M K' , '23 JAN 1932')
+(1, 'SOHEL', 'RANA', 20000, 'CSE', '20 SEPT 2024'),
+(2, 'FAHIM', 'RAHMAN', 20000, 'CSE', '20 SEPT 2024'),
+(3, 'SABIT', 'RAIHAN', 30000, 'CSE', '10 OCT 2023'),
+(4, 'TAHSIN', 'SIDDIK', 424443, 'CSE', '2 OCT 2023'),
+(5, 'AKIB', 'AHMED', 32221, 'EEE', '3 JAN 1993'),
+(6, 'SOMRAT', 'BOY', 73554, 'BSC', '6 JUN 1971'),
+(7, 'SHOWRAB', 'KHORMOKAR', 73554, 'DRUG D', '21 JAN 100'),
+(8, 'BIJOY' ,'MOHOITRO', 73534, 'M K' , '23 JAN 1932')
