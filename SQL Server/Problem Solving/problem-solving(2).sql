5ee7b9e067169c5471cabec42f8ba96bef0846cd
SOHEL RANA <sohelrana104322@gmail.com>
2024-09-16 16:18:53 +0600

    fixed one query

--- SQL Server/Problem Solving/problem-solving(2).sql
+++ SQL Server/Problem Solving/problem-solving(2).sql
@@ -120,8 +120,7 @@ WHERE first_name LIKE 'k%';
 
 
 /*----------
-Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘r’
-and contains seven alphabets.-----------*/SELECT *FROM workerInfoWHERE first_name LIKE '%r';/*-------Write an SQL query to find the position of the alphabet (‘n’) in the FIRST_NAME
+Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘r’ and contains seven alphabets-----------*/SELECT *FROM workerInfoWHERE first_name LIKE '%r'AND DATALENGTH(first_name) = 7;/*-------Write an SQL query to find the position of the alphabet (‘n’) in the FIRST_NAME
 column ‘Sanjoy’ from Worker table
 ---------*/
 
