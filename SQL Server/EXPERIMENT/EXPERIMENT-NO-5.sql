1437bdb6a408985ddf90258e4640f87d4a864cea
SOHEL RANA <sohelrana104322@gmail.com>
2024-10-31 22:32:03 +0600

    solve experiment-5 probles

--- SQL Server/EXPERIMENT/EXPERIMENT-NO-5.sql
+++ SQL Server/EXPERIMENT/EXPERIMENT-NO-5.sql
@@ -153,5 +153,165 @@ INNER JOIN salesman S
 ON O.salesman_id = S.salesman_id;
 
 SELECT *
-FROM Order_Salesman_Customer_Name
+FROM Order_Salesman_Customer_Name;
+
+
+/* ----
+7. Write a query to create a view that finds the salesman who has the customer with the
+highest order of a day 
+----*/
+CREATE VIEW highest_order_of_a_day AS
+SELECT S.*, Order_Count
+FROM salesman S
+INNER JOIN
+(SELECT TOP 1 salesman_id, COUNT(*) AS Order_Count
+FROM orders
+WHERE ord_date = '2012-09-10'
+GROUP BY salesman_id) AS Temp
+ON S.salesman_id = Temp.salesman_id;
+
+SELECT *
+FROM highest_order_of_a_day;
+
+
+/* ---
+8. Write a query to create a view that finds the salesman who has the customer with the
+highest order at least 3 times on a day
+---- */
+
+CREATE VIEW At_Least_Three_Order AS
+SELECT S.*, Order_Count
+FROM salesman S
+INNER JOIN
+(SELECT TOP 1 salesman_id, COUNT(*) AS Order_Count
+FROM orders
+WHERE ord_date = '2012-09-10'
+GROUP BY salesman_id
+HAVING COUNT(*) >=3) AS Temp
+ON S.salesman_id = Temp.salesman_id;
+
+SELECT *
+FROM At_Least_Three_Order;
+
+
+/* ---
+9. Write a query to create a view that shows all of the customers who have the highest
+grade
+--- */
+CREATE VIEW Highest_Grade AS
+SELECT *
+FROM customer
+WHERE grade = (SELECT MAX(grade) FROM customer);
+
+SELECT *
+FROM Highest_Grade;
+
+
+/* ---
+10. Write a query to create a view that shows the number of the salesman in each city
+--- */
+CREATE VIEW Salesman_In_Same_City AS
+SELECT city, COUNT(*) AS No_Of_Salesman
+FROM customer
+GROUP BY city
+ORDER BY No_Of_Salesman OFFSET 0 ROWS;
+
+SELECT *
+FROM Salesman_In_Same_City
+
+/*'OFFSET 0 ROWS' to specify that no rows need to be skipped and start 
+from the first row */
+
+
+/* ---
+11. Write a query to create a view that shows the average and total orders for each
+salesman after his or her name. (Assume all names are unique)--- */
+CREATE VIEW avg_total_orders AS
+SELECT S.salesman_id,  S.name, Average_Order, Total_Order
+FROM salesman S
+INNER JOIN
+(SELECT salesman_id, SUM(purch_amt) AS Total_Order, AVG(purch_amt) AS Average_Order
+FROM orders
+GROUP BY salesman_id) AS temp
+ON S.salesman_id = temp.salesman_id;
+
+SELECT *
+FROM avg_total_orders;
+
+
+/* ---
+12. Write a query to create a view that shows each salesman with more than one
+customers
+--- */
+CREATE VIEW More_Than_One_Customers AS
+SELECT S.*, Tota_Customers
+FROM salesman S
+INNER JOIN
+(SELECT salesman_id, COUNT(*) AS Tota_Customers
+FROM customer 
+GROUP BY salesman_id
+HAVING COUNT(*) > 1) AS temp
+ON S.salesman_id = temp.salesman_id;
+
+SELECT *
+FROM More_Than_One_Customers;
+
+
+/* ---
+13. Write a query to create a view that shows all matches of customers with salesman
+such that at least one customer in the city of customer served by a salesman in the city
+of the salesman
+--- */
+
+
+
+
+/* ---
+14. Write a query to create a view that shows the number of orders in each day
+----*/
+CREATE VIEW No_Of_Order_Each_Day AS
+SELECT ord_date, COUNT(*) AS No_Of_Orders
+FROM orders
+GROUP BY ord_date;
+
+SELECT *
+FROM No_Of_Order_Each_Day;
+
+
+/* ---
+15. Write a query to create a view that finds the salesmen who issued orders on October
+10th, 2012
+---*/
+CREATE VIEW Salesman_Order_Date AS
+SELECT S.*, ord_date
+FROM salesman S
+INNER JOIN
+(SELECT salesman_id, ord_date
+FROM orders
+WHERE ord_date = '2012-10-10') AS temp
+ON S.salesman_id = temp.salesman_id;
+
+SELECT *
+FROM Salesman_Order_Date;
+
+
+/* ---
+16. Write a query to create a view that finds the salesmen who issued orders on either
+August 17th, 2012 or October 10th, 2012
+----*/
+CREATE VIEW Salesman_Order_Date_Two_Times AS
+SELECT S.*, ord_date
+FROM salesman S
+INNER JOIN
+(SELECT salesman_id, ord_date
+FROM orders
+WHERE ord_date = '2012-10-10' OR ord_date ='2012-08-17') AS temp
+ON S.salesman_id = temp.salesman_id;
+
+SELECT *
+FROM Salesman_Order_Date_Two_Times;
+
+
+
+
 
