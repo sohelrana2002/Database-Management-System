54976f939066d7fdc51962d05dc94cb9bfe89ace
SOHEL RANA <sohelrana104322@gmail.com>
2024-11-19 10:57:13 +0600

    update store procedure file

--- SQL Server/Problem Solving/stored-procedure.sql
+++ SQL Server/Problem Solving/stored-procedure.sql
@@ -65,5 +65,72 @@ EXEC spStudent;
 
 
 /*---------view stored procedure-------*/
-sp_helptext spStudent;
+EXEC sp_helptext spStudent;
 /* NOTE: output : The text for object 'spStudent' is encrypted.*/
+
+
+/*----output parameter stored procedure----*/
+CREATE PROC outputParameterStudent
+@Email VARCHAR(50),
+@StudentCount INT OUTPUT
+AS
+BEGIN
+	SELECT @StudentCount = COUNT(*)
+	FROM student
+	WHERE email = @Email
+END;
+
+
+/* -----execution stored procedure----*/
+DECLARE @TotalCount INT
+EXEC outputParameterStudent 'sohelrana110979@gmail.com', @TotalCount OUTPUT
+PRINT @TotalCount;
+
+/* -----execution stored procedure with condition----*/
+DECLARE @TotalCount INT
+EXEC outputParameterStudent 'sohelrana110979@gmail.com', @TotalCount 
+
+IF(@TotalCount IS NULL)
+	PRINT 'Empty'
+ELSE
+	PRINT @TotalCount
+
+/*Note: if we cant use OUTPUT KEYWORD when EXEC then get null value*/
+/* result: Empty*/
+
+/*--using sp_help view the information about stored procedure---*/
+EXEC sp_help outputParameterStudent;
+
+
+/*--view the dependency of the stored procedure---*/
+EXEC sp_depends outputParameterStudent;
+
+/* ----RETURN VALUE---*/
+CREATE PROC returnStudent
+AS
+BEGIN
+	RETURN(SELECT COUNT(*) FROM student)
+END
+
+
+/*---execute return value procedure--*/
+DECLARE @result INT
+EXEC @result = returnStudent
+PRINT @result
+
+
+/*---output parameter----*/
+CREATE PROC outStudent
+@id INT,
+@Name VARCHAR(50) OUTPUT
+AS
+BEGIN
+	SELECT @Name = name
+	FROM student
+	WHERE id = @id
+END
+
+/*--execute output parameter--*/
+DECLARE @StudentName VARCHAR(50)
+EXEC outStudent 222311057, @StudentName OUTPUT
+PRINT 'Student Name Is: ' + @StudentName
